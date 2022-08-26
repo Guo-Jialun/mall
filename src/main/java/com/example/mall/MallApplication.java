d3770a4ec986dd8ddb068c107ce38e8f56c32b8e
86132 <2642440908@qq.com>
2022-08-26 17:23:30 +0800

    second commit

--- src/main/java/com/example/mall/MallApplication.java
+++ src/main/java/com/example/mall/MallApplication.java
@@ -2,8 +2,18 @@ package com.example.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import java.util.TimeZone;
+
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableScheduling
+@MapperScan(basePackages = "com.example.mall.mapper")
+
 public class MallApplication {
 
     public static void main(String[] args) {
